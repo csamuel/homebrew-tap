1fe48b7fe17e897dee10339b274fdc53f5ca0943
Chris Samuel <csamuel@gmail.com>
2023-01-25 14:45:31 -0600

    sacd-ripper - update to latest

--- Formula/sacd-extract.rb
+++ Formula/sacd-extract.rb
@@ -1,8 +1,8 @@
 class SacdExtract < Formula
   desc "Improved sacd_extract"
   homepage "https://github.com/csamuel/sacd-ripper"
-  url "https://github.com/csamuel/sacd-ripper/archive/refs/tags/v0.1-arm64.tar.gz"
-  sha256 "bb887d41a465015667b48faf31b078da64cfb4538a9d745fdac886861c2a98c4"
+  url "https://github.com/csamuel/sacd-ripper/archive/refs/tags/v0.1.1-arm64.tar.gz"
+  sha256 "4c91645ec9039327ca1f7688aa4cf9a6ea6622a54c8699f945349007b2fafb65"
   license "GPL-2.0-only"
 
   head "https://github.com/csamuel/sacd-ripper.git", branch: "master"
